aff97229720c592b2f26f642f66ef4998313abf4
Iarley-01 <yarleyalves2016@gmail.com>
2024-02-19 13:50:40 -0300

    criando estrutura do menu-title component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MenuTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
